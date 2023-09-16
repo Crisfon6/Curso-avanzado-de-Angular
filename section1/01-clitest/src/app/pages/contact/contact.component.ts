dc1f3c0338ff46d2023b23af9dcfc72f53eefd33
crisfon6 <disruptivecrisfon6@gmail.com>
2023-09-16 17:07:17 -0500

    first commit

---
+++ section1/01-clitest/src/app/pages/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+ constructor(authService:AuthService){
+ }
+}
