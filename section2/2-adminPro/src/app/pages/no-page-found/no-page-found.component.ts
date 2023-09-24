6029a0e50e5343a83489cbe40285825abbfbab44
crisfon6 <disruptivecrisfon6@gmail.com>
2023-09-23 21:38:22 -0500

    feat: se definen las rutas y los templates de las rutas

--- section2/2-adminPro/src/app/pages/no-page-found/no-page-found.component.ts
+++ section2/2-adminPro/src/app/pages/no-page-found/no-page-found.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-page-found',
   templateUrl: './no-page-found.component.html',
-  styles: [
+  styleUrls: [
+    './no-page-found.component.css'
   ]
 })
 export class NoPageFoundComponent {
-
+  year = new Date().getFullYear();
 }
